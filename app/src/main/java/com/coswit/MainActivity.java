d098ff8229f0378d809c4d8fd748a0b954739ff7
zhj <zhengjingytz@126.com>
2019-12-02 11:13:10 +0800

    scroller 原理测试

--- app/src/main/java/com/coswit/MainActivity.java
+++ app/src/main/java/com/coswit/MainActivity.java
@@ -1,11 +1,14 @@
 package com.coswit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.coswit.View.CircleNumView;
 import com.coswit.View.GraphicView;
+import com.coswit.scroller.ScrollerTestActivity;
 
 import java.util.ArrayList;
 
@@ -15,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.cn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScrollerTestActivity.class));
+            }
+        });
 
         GraphicView view = findViewById(R.id.gv);
         ArrayList<GraphicView.PointInfo> infos = new ArrayList<>();
